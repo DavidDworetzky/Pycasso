5a00d730056b651f7ad251b7b52634fffcfa63da
David Dworetzky <djdworetz@gmail.com>
2019-02-20 10:52:55 -0600

    Refactored Neural_Transfer.py

--- Pycasso/Core/Neural_Transfer.py
+++ Pycasso/Core/Neural_Transfer.py
@@ -2,24 +2,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
 from PIL import Image
 import matplotlib.pyplot as plt
-
 import torchvision.transforms as transforms
 import torchvision.models as models
 from io import BytesIO
 import base64
-
 import copy
 
-#Helper functions for image processing
-def image_loader(image_data):
+#CONSTANTS
+# desired depth layers to compute style/content losses :
+content_layers_default = ['conv_4']
+style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+#Helper functions for image processing -> loading
+def image_loader(image_data, loader, device):
+    data = {}
     data['image'] = image_data
     image = Image.open(BytesIO(base64.b64decode(data)))
     # fake batch dimension required to fit network's input dimensions
     image = loader(image).unsqueeze(0)
     return image.to(device, torch.float)
+
 #Get Optimizer for Neural Network
 def get_input_optimizer(input_img):
     # this line to show that input is a parameter that requires a gradient
@@ -44,6 +48,67 @@ def gram_matrix(input):
     G = torch.mm(features, features.t())  # compute the gram product
     #return normalized gram matrix
     return G.div(a * b * c * d)
+
+def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
+                               style_img, content_img,
+                               content_layers=content_layers_default,
+                               style_layers=style_layers_default):
+    cnn = copy.deepcopy(cnn)
+
+    # normalization module
+    normalization = Normalization(normalization_mean, normalization_std).to(device)
+
+    # just in order to have an iterable access to or list of content/syle
+    # losses
+    content_losses = []
+    style_losses = []
+
+    # assuming that cnn is a nn.Sequential, so we make a new nn.Sequential
+    # to put in modules that are supposed to be activated sequentially
+    model = nn.Sequential(normalization)
+
+    i = 0  # increment every time we see a conv
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = 'conv_{}'.format(i)
+        elif isinstance(layer, nn.ReLU):
+            name = 'relu_{}'.format(i)
+            # The in-place version doesn't play very nicely with the ContentLoss
+            # and StyleLoss we insert below. So we replace with out-of-place
+            # ones here.
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = 'pool_{}'.format(i)
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = 'bn_{}'.format(i)
+        else:
+            raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
+        model.add_module(name, layer)
+
+        if name in content_layers:
+            # add content loss:
+            target = model(content_img).detach()
+            content_loss = ContentLoss(target)
+            model.add_module("content_loss_{}".format(i), content_loss)
+            content_losses.append(content_loss)
+
+        if name in style_layers:
+            # add style loss:
+            target_feature = model(style_img).detach()
+            style_loss = StyleLoss(target_feature)
+            model.add_module("style_loss_{}".format(i), style_loss)
+            style_losses.append(style_loss)
+
+    # now we trim off the layers after the last content and style losses
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            break
+
+    model = model[:(i + 1)]
+
+    return model, style_losses, content_losses
 
 def run_style_transfer(cnn, normalization_mean, normalization_std,
                        content_img, style_img, input_img, num_steps=300,
@@ -117,14 +182,16 @@ class Neural_Transfer:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.image_size = image_size
         # scale imported image and transform it into a torch tensor
-        self.loader transforms.Compose([transforms.Resize((self.image_size, self.image_size)),  transforms.ToTensor()])
+        self.loader = transforms.Compose([transforms.Resize((self.image_size, self.image_size)), transforms.ToTensor()])
         #initialize style and content image 
-        self.style_image = image_loader(style_image)
-        self.content_image = image_loader(content_image)
-        self.cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
-        self.cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
-        self.cnn = models.vgg19(pretrained=True).features.to(device).eval()
+        self.style_image = image_loader(style_image, self.loader, self.device)
+        self.content_image = image_loader(content_image, self.loader, self.device)
+        self.cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(self.device)
+        self.cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(self.device)
+        self.cnn = models.vgg19(pretrained=True).features.to(self.device).eval()
         
     def run_transfer(self, num_steps):
-        output = run_style_transfer(self.cnn, self.cnn_normalization_mean, self.normalization_std, num_steps = num_steps)
+        input_image = self.content_image.clone()
+        output = run_style_transfer(self.cnn, self.cnn_normalization_mean, self.cnn_normalization_std, 
+        self.content_image, self.style_image, input_image, num_steps = num_steps)
         return output
