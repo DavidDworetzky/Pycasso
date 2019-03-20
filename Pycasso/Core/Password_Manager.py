5a57b8e775f3c8d32c78cdf9ffee0d720710bbf4
David Dworetzky <djdworetz@gmail.com>
2019-03-19 23:22:44 -0500

    Add basic password manager for hashing user passwords

---
+++ Pycasso/Core/Password_Manager.py
@@ -0,0 +1,19 @@
+import crypt
+import os
+import random
+import string
+from hmac import compare_digest as compare_hash
+import hashlib
+
+
+class Password_Manager:
+    def __init__(self, salt=None, rounds=None):
+        self.salt = salt
+        self.rounds = rounds
+
+    def sha512_hash(self,password):
+        return hashlib.sha512(self.salt + password).hexdigest()
+
+    #verifies a hashed password
+    def sha512_verify(self,password, hash):
+        return self.sha512_hash(password) == hash
