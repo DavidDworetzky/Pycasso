ea849e7fd6c434cb09d2a016822abbe2e57b0399
David Dworetzky <djdworetz@gmail.com>
2019-03-22 21:48:27 -0500

    Add core unit tests to pycasso. Tests for password manager

---
+++ Test/Unit/Core.py
@@ -0,0 +1,27 @@
+import unittest
+from Pycasso.Core.Password_Manager import Password_Manager as PM
+
+#Fixtures
+TEST_PASSWORD = "Password!"
+def get_password_manager():
+    manager = PM()
+    return manager
+
+#Test Password Manager
+class TestPasswordManager(unittest.TestCase):
+
+    def test_crypt(self):
+        manager = get_password_manager()
+        hash = manager.sha512_hash(TEST_PASSWORD)
+        #trivial assert
+        self.assertTrue(hash != "")
+
+    def test_verifyhash(self):
+        #hash password and then verify hash works
+        manager = get_password_manager()
+        hash = manager.sha512_hash(TEST_PASSWORD)
+        #trivial assert
+        self.assertTrue(hash != "")
+        #assert hash verification
+        self.assertTrue(manager.sha512_verify(TEST_PASSWORD, hash))
+        
