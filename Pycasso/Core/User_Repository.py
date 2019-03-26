3a045607ed642f4f6406471077d32ecf415f61b2
David Dworetzky <djdworetz@gmail.com>
2019-03-26 00:28:38 -0500

    Add password hashing to user_repo

--- Pycasso/Core/User_Repository.py
+++ Pycasso/Core/User_Repository.py
@@ -3,11 +3,14 @@ import uuid
 from tinydb.operations import set
 import datetime
 from enum import Enum
+from Pycasso.Core.Password_Manager import Password_Manager as PM
 
 class User_Repository:
-    def __init__(self, file_path):
+    def __init__(self, file_path, password_salt = ""):
         self.file_path = file_path
         self.db = TinyDB(self.file_path)
+        self.password_salt = password_salt
+        self.pw_manager = PM(salt = self.password_salt)
 
     #takes a job request object and inserts it into our database
     def create_user(self, user):
@@ -15,7 +18,8 @@ class User_Repository:
         user_id_string = str(user_id)
         create_date = datetime.datetime.now()
         create_date_string = str(create_date)
-        user = {'first' : user['first'], 'last' : user['last'], 'id': user_id_string, 'create_date': create_date_string}
+        hash = self.pw_manager.sha512_hash(user['password'])
+        user = {'first' : user['first'], 'last' : user['last'], 'id': user_id_string, 'create_date': create_date_string, 'hash' : hash}
         self.db.insert(user)
         return user
     def delete_user(self, user_id):
