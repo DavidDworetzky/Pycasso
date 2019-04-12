11e2407b7908ca933ed537f2b0a73e43d6f63903
David Dworetzky <djdworetz@gmail.com>
2019-04-11 23:21:57 -0500

    Gating work on multiprocess queue for synchronizing jobs

---
+++ Pycasso/Core/Process_Queue.py
@@ -0,0 +1,23 @@
+import multiprocessing as mp
+
+class Process_Queue:
+    def __init__(self, Queue, Func, Num_Process = 4):
+        self.process = Num_Process
+        self.f = Func
+        self.Queue = mp.Queue()
+        #load our queue with jobs
+        for item in Queue:
+            self.Queue.put(item)
+        self.Processes = []
+        #create initial processes
+
+    def Enqueue(self, queue_item):
+        self.Queue.put(queue_item)
+
+    def Dequeue(self, queue):
+        return self.Queue.get()
+
+    def Start_Processes(self):
+        return
+
+    
